44ed2578b0bb6311eeb1378296ccacba7ddc365d
Mohammad Nayan <mohammad.nayan.official@gmail.com>
2026-04-12 05:00:27 +0000

    Initial commit

---
+++ scripts/commands/goiadmin.js
@@ -0,0 +1,25 @@
+module.exports.config = {
+  name: "goiadmin",
+    version: "1.0.0",
+    permission: 0,
+    credits: "nayan",
+    description: "mention",
+    prefix: true,
+    category: "user",
+    usages: "tag",
+    cooldowns: 5,
+};
+module.exports.handleEvent = function({ api, event }) {
+
+  
+  if (event.senderID !== "100000959749712") {
+    var aid = ["100000959749712"];
+    for (const id of aid) {
+    if ( Object.keys(event.mentions) == id) {
+      var msg = ["Mantion_দিস না _নয়ন বস এর মন মন ভালো নেই আস্কে-!💔🥀", "- আমার সাথে কেউ সেক্স করে না থুক্কু টেক্স করে নাহ🫂💔", "আমার একটা প্রিয়র খুব দরকার কারন আমার চোখে পানি আসার আগে নাকে সর্দি চলে আসে🤣🤣","এত মেনশন না দিয়ে বক্স আসো হট করে দিবো🤷‍ঝাং 😘🥒"," Mantion_দিলে চুম্মাইয়া ঠুটের কালার change কইরা,লামু 💋😾😾🔨","এতু ইমুশানাল কথা বলো তল দেশ দিয়ে অজরে বৃষ্টি হচ্ছে আমার 😭😭","নয়ন বস এখন  বিজি জা বলার আমাকে বলতে পারেন_!!😼🥰","এতো মিনশন নাহ দিয়া সিংগেল নয়ন রে একটা গফ দে 😒 😏","Mantion_না দিয়ে সিরিয়াস প্রেম করতে চাইলে ইনবক্স","মেনশন দিসনা পারলে একটা গফ দে","Mantion_দিস না বাঁলপাঁক্না নয়ন প্রচুর বিজি 🥵🥀🤐","চুমু খাওয়ার বয়স টা  চকলেট🍫খেয়ে উড়িয়ে দিলাম🤗"];
+      return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
+    }
+    }}
+};
+module.exports.run = async function({}) {
+}
